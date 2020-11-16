71484f0e066b0be701f49eb0c247ca440f19ed3e
Oleg Semenov <wemonsh.gg@gmail.com>
2020-11-16 17:43:29 +0300

    productive invite link update

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -12,7 +12,8 @@ class RedirectController extends Controller
         if ($user->invite === null) {
             $user->invite = 1;
             $user->save();
-            return redirect()->to('https://t.me/joinchat/CgQ3Wh0qdmwPfeNU3MHopg');
+            //return redirect()->to('https://t.me/joinchat/CgQ3Wh0qdmwPfeNU3MHopg');
+            return redirect()->to('https://t.me/joinchat/Ga6OUUWn7gDJPg0LXx_NYA');
         } else {
             echo 'Приглашение уже было использовано!';
         }
